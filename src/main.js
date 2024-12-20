def37f0351a605050fb245fe1b952753cbd31653
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2024-12-20 15:29:48 +0100

    add rest of svg and improted js-files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/header';
+import './js/hero';
+import './js/about';
+import './js/benefits';
+import './js/projects';
+import './js/fag';
+import './js/covers';
+import './js/reviews';
+import './js/reviews';
+import './js/modal-w';
